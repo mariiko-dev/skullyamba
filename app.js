cf89e1fa9b1a28c65bbdb35f8497e3bc377dcf33
skullyamba <163741396+skullyamba@users.noreply.github.com>
2024-10-25 21:27:52 +0300

    daniku-pizda

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 function getRandomChoice() {
-    const choices = ["Никита", "Даник"];
+    const choices = ["Даник", "Даник"];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
   }
   
   const computerChoice = getRandomChoice();
-  console.log(`Кто получит пизды: ${computerChoice}`);
+  console.log(`Кто получит пизды завтра утром: ${computerChoice}`);
